b87935145fb117ff2ae0eeaf879d067bcf0f1dba
higorini <108697425+higorini@users.noreply.github.com>
2023-11-16 01:06:26 -0300

    Info modal created

---
+++ assets/js/modal.js
@@ -0,0 +1,11 @@
+const infoButton = document.getElementById("infoButton");
+const modal = document.getElementById("modal");
+const closeButton = document.querySelector(".close");
+
+infoButton.addEventListener("click", () => {
+  modal.style.display = "block";
+});
+
+closeButton.addEventListener("click", () => {
+  modal.style.display = "none";
+});
--- assets/js/pagination.js
+++ assets/js/pagination.js
@@ -6,35 +6,42 @@ const currentPage = 1;
 
 function calcularNumeroDePaginas(limit, totalPokemons) {
   const quantidadeDePokemons = Math.ceil(totalPokemons / limit);
-  console.log(quantidadeDePokemons)
+  console.log(quantidadeDePokemons);
   return quantidadeDePokemons;
 }
 
 function criarPaginacao(totalPokemons) {
-    const pagination = document.getElementById("pagination");
-    const porPagina = 15;
-    const numeroDePaginas = calcularNumeroDePaginas(limit, totalPokemons);
-  
-    for (let i = 0; i < numeroDePaginas; i++) {
-      const pageNumber = i + 1;
-      const newButton = document.createElement("button");
-      const offset = i * porPagina;
+  const pagination = document.getElementById("pagination");
+  const porPagina = 15;
+  const numeroDePaginas = calcularNumeroDePaginas(limit, totalPokemons);
 
-      function formatPageNumber(id) {
-        return String(id).padStart(2, "0");
-      }
+  for (let i = 0; i < numeroDePaginas; i++) {
+    const pageNumber = i + 1;
+    const newButton = document.createElement("button");
+    const offset = i * porPagina;
 
-      newButton.textContent = formatPageNumber(pageNumber)
-      newButton.dataset.offset = offset;
-  
-      newButton.addEventListener("click", function () {
-        const newOffset = this.dataset.offset;
-        showLoading();
-        loadPokemon(newOffset, limit)
-      });
-  
-      pagination.appendChild(newButton);
+    function formatPageNumber(id) {
+      return String(id).padStart(2, "0");
     }
+
+    if (pageNumber === 1) {
+      newButton.classList.add("active");
+    }
+
+    newButton.textContent = formatPageNumber(pageNumber);
+    newButton.dataset.offset = offset;
+
+    newButton.addEventListener("click", function () {
+      const newOffset = this.dataset.offset;
+      showLoading();
+      loadPokemon(newOffset, limit);
+      const buttons = pagination.querySelectorAll("button");
+      buttons.forEach((button) => button.classList.remove("active"));
+      this.classList.add("active");
+    });
+
+    pagination.appendChild(newButton);
   }
+}
 
 criarPaginacao(totalPokemons);
